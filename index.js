ff0ed3b52b2315abd2507f8dd1f99404389b489f
Lautaro-Dominguez <lautaro.e.dominguez@gmail.com>
2021-09-24 18:00:51 -0300

    Cambios

---
+++ index.js
@@ -0,0 +1,10 @@
+$("#btnrayas").click( ()=>{
+    $("#menudesplegable").append(
+        `<a href="index.html" class="headertext"><p>Página Principal</p></a>
+        <a href="tienda.html" class="headertext"><p>Tienda Online</p></a>`
+)}
+)
+
+
+
+
